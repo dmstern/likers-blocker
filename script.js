17319a64665f4373b9e6e8a4b117fa87efc31379
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2020-02-23 09:00:32 +0100

    improve button click with new tab

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var blockIcon =
   '<svg viewBox="0 0 24 24" class="r-9ilb82 r-4qtqp9 r-yyyyoo r-1q142lx r-1xvli5t r-zso239 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M12 1.25C6.072 1.25 1.25 6.072 1.25 12S6.072 22.75 12 22.75 22.75 17.928 22.75 12 17.928 1.25 12 1.25zm0 1.5c2.28 0 4.368.834 5.982 2.207L4.957 17.982C3.584 16.368 2.75 14.282 2.75 12c0-5.1 4.15-9.25 9.25-9.25zm0 18.5c-2.28 0-4.368-.834-5.982-2.207L19.043 6.018c1.373 1.614 2.207 3.7 2.207 5.982 0 5.1-4.15 9.25-9.25 9.25z"></path></g></svg>';
+var apiUrlBlock = "https://ichbinhier-twittertools.herokuapp.com/blockapi/";
 
 function tryToAccessDOM(callback, elementToExpectSelector, context = document) {
   var elementToExpect = null;
@@ -49,9 +50,14 @@ function addBlockButton() {
     blockButton.style.marginRight = "1rem";
     blockButton.dataset.testid = "blockAll";
     blockButton.innerHTML = followButton.innerHTML;
-    blockButton.querySelector("div > span > span").innerHTML =
-      "Alle Blockieren";
+    var blockButtonLabel = blockButton.querySelector("div > span > span")
+    blockButtonLabel.innerHTML = "Alle Blockieren";
     blockButton.target = "_blank";
+
+    var usernamesList = getUsernames().join(":");
+    var requestUrl = `${apiUrlBlock}profile_urls=${usernamesList}`;
+    blockButton.href = requestUrl;
+
     let topbar = document.querySelector(
       "main > div > div > div > div > div > div"
     );
@@ -66,15 +72,14 @@ function addBlockButton() {
       blockButton
     ).borderBottomColor;
 
-    blockButton.addEventListener("click", function() {
+    blockButton.addEventListener("click", function(event) {
       var confirmed = confirm(
         "Bist du sicher, dass du alle, die diesen Tweet geliked haben blockieren möchtest?"
       );
-
-      if (confirmed) {
-        var usernamesList = getUsernames().join(":");
-        var requestUrl = `https://ichbinhier-twittertools.herokuapp.com/blockapi/profile_urls=${usernamesList}`;
-        window.location.href = requestUrl;
+      
+      if (!confirmed) {
+        event.preventDefault();
+        event.stopPropagation();
       }
 
     });
@@ -82,7 +87,7 @@ function addBlockButton() {
   }, "[data-testid*=follow]");
 }
 
-// for we are on the likes page:
+// for when we are on the likes page:
 addBlockButton();
 
 // For every other page: try it on click again:
