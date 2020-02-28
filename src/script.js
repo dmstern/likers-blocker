3d6973bbbde9bc8df9ad8f3b05770e870363b85f
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2020-02-28 12:48:11 +0100

    improve confirm message & fix wrong checkmark animation

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 var blockIcon =
   '<svg viewBox="0 0 24 24" class="r-9ilb82 r-4qtqp9 r-yyyyoo r-1q142lx r-1xvli5t r-zso239 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M12 1.25C6.072 1.25 1.25 6.072 1.25 12S6.072 22.75 12 22.75 22.75 17.928 22.75 12 17.928 1.25 12 1.25zm0 1.5c2.28 0 4.368.834 5.982 2.207L4.957 17.982C3.584 16.368 2.75 14.282 2.75 12c0-5.1 4.15-9.25 9.25-9.25zm0 18.5c-2.28 0-4.368-.834-5.982-2.207L19.043 6.018c1.373 1.614 2.207 3.7 2.207 5.982 0 5.1-4.15 9.25-9.25 9.25z"></path></g></svg>';
 var checkmarkIcon = `<?xml version="1.0" encoding="UTF-8"?><svg width="45.255mm" height="37.707mm" version="1.1" viewBox="0 0 45.255 37.707" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><metadata><rdf:RDF><cc:Work rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/><dc:title/></cc:Work></rdf:RDF></metadata><g transform="translate(-54.843 -79.398)"><path d="m56.872 99.051 16.455 13.496 24.244-31.185"/></g></svg>`;
-var closeIcon = '<svg viewBox="0 0 24 24" class="r-13gxpu9 r-4qtqp9 r-yyyyoo r-1q142lx r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path></g></svg>';
+var closeIcon =
+  '<svg viewBox="0 0 24 24" class="r-13gxpu9 r-4qtqp9 r-yyyyoo r-1q142lx r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path></g></svg>';
 
 var apiUrlBlock = "https://ichbinhier-twittertools.herokuapp.com/blocklists";
 var urlLengthMax = 2000;
@@ -160,64 +161,7 @@ function addBlockButton() {
 
       // scroll down to get more users:
       var initBlocking = function(users, requestUrl, popup) {
-        var confirmMessage = `<p>Willst du alle ${
-          users.length
-        } Nutzer blockieren? <br/>
-          Evtl. musst du in deinem Browser Popups für twitter.com erlauben.</p>`;
-
-        var confirmButton = blockButton.cloneNode(true);
-        confirmButton.classList.add("lb-confirm-button");
-        confirmButton.classList.remove("lb-block-button");
-        confirmButton.querySelector("div > span").remove();
-        confirmButton.querySelector("div > span > span").innerText = "OK";
-
-        var checkbox = document.createElement("input");
-        var label = document.createElement("label");
-        var labelWrapper = document.createElement("div");
-        labelWrapper.appendChild(label);
-
-        var loadingInfo = popup.querySelector(".lb-label");
-        var confirmMessageElement = loadingInfo.cloneNode();
-        confirmMessageElement.classList.remove("lb-collecting");
-        confirmMessageElement.classList.add("lb-confirm-message");
-        confirmMessageElement.innerHTML = confirmMessage;
-        popup.appendChild(confirmMessageElement);
-
-        checkbox.type = "checkbox";
-        checkbox.classList.add("lb-checkbox");
-        confirmMessageElement.appendChild(labelWrapper);
-        label.innerHTML = "<span>Auch alle Retweeter blockieren?</span>";
-        retweetersNotice = document.createElement("span");
-        retweetersNotice.classList.add("lb-info");
-        retweetersNotice.title =
-          "Beinhaltet nur direkte Retweeter ohne Kommentar";
-        retweetersNotice.textContent = "🛈";
-
-        label.prepend(checkbox);
-        labelWrapper.appendChild(retweetersNotice);
-        confirmMessageElement.appendChild(confirmButton);
-
-        var checkmark = document.createElement("span");
-        checkmark.classList.add("lb-checkmark");
-        loadingInfo.appendChild(checkmark);
-        checkmark.style.background = highlightColor;
-        checkmark.innerHTML = checkmarkIcon;
-
-        confirmButton.addEventListener("click", () => {
-          var tweetParam = checkbox.checked ? `&tweet_id=${tweetId}` : "";
-          window.open(`${requestUrl}${tweetParam}`, "_blank");
-          closePopup(popupWrapper, scrollList);
-        });
-
-        var loadingIndicator = popup.querySelector(".lb-loading");
-        loadingIndicator.addEventListener("animationiteration", () => {
-          animationIterationCounter++;
-
-          // only continue when indicator is on the right side:
-          if (animationIterationCounter % 2 == 0) {
-            popup.classList.add("lb-collected");
-          }
-        });
+        var checkmark = popup.querySelector(".lb-checkmark");
 
         checkmark.addEventListener("transitionend", () => {
           popup.classList.add("lb-confirm");
@@ -257,6 +201,69 @@ function addBlockButton() {
         popupWrapper.classList.remove("lb-hide");
       }, 250);
 
+      // add confirmMessage to dialog:
+      var confirmMessage = `
+      <h3> Nutzer gefunden. Alle blockieren?</h3>
+      <p>Evtl. musst du in deinem Browser Popups für twitter.com erlauben.</p>`;
+
+      var confirmButton = blockButton.cloneNode(true);
+      confirmButton.classList.add("lb-confirm-button");
+      confirmButton.classList.remove("lb-block-button");
+      confirmButton.querySelector("div > span").remove();
+      confirmButton.querySelector("div > span > span").innerText = "OK";
+
+      var checkbox = document.createElement("input");
+      var label = document.createElement("label");
+      var labelWrapper = document.createElement("div");
+      labelWrapper.appendChild(label);
+
+      var loadingInfo = popup.querySelector(".lb-label");
+      var confirmMessageElement = loadingInfo.cloneNode();
+      confirmMessageElement.classList.remove("lb-collecting");
+      confirmMessageElement.classList.add("lb-confirm-message");
+      confirmMessageElement.innerHTML = confirmMessage;
+      popup.appendChild(confirmMessageElement);
+
+      checkbox.type = "checkbox";
+      checkbox.classList.add("lb-checkbox");
+      confirmMessageElement.appendChild(labelWrapper);
+      label.innerHTML = "<span>Auch alle Retweeter blockieren?</span>";
+      retweetersNotice = document.createElement("span");
+      retweetersNotice.classList.add("lb-info");
+      retweetersNotice.title =
+        "Beinhaltet nur direkte Retweeter ohne Kommentar";
+      retweetersNotice.textContent = "🛈";
+
+      label.prepend(checkbox);
+      labelWrapper.appendChild(retweetersNotice);
+      confirmMessageElement.appendChild(confirmButton);
+
+      var checkmark = document.createElement("span");
+      checkmark.classList.add("lb-checkmark");
+      loadingInfo.appendChild(checkmark);
+      checkmark.style.background = highlightColor;
+      checkmark.innerHTML = checkmarkIcon;
+
+      confirmButton.addEventListener("click", () => {
+        var tweetParam = checkbox.checked ? `&tweet_id=${tweetId}` : "";
+        window.open(`${requestUrl}${tweetParam}`, "_blank");
+        closePopup(popupWrapper, scrollList);
+      });
+
+      var loadingIndicator = popup.querySelector(".lb-loading");
+      loadingIndicator.addEventListener("animationiteration", () => {
+        animationIterationCounter++;
+
+        // only continue when indicator is on the right side:
+        if (
+          animationIterationCounter % 2 != 0 &&
+          popup.classList.contains("lb-check")
+        ) {
+          popup.classList.add("lb-collected");
+        }
+      });
+
+      // add closeButton:
       var closeButton = document.createElement("div");
       closeButton.role = "button";
       closeButton.innerHTML = closeIcon;
@@ -293,7 +300,12 @@ function addBlockButton() {
         var reachedUrlLengthMax = requestUrl.length > urlLengthMax - 100;
 
         if (scrolledToBottom || scrollListIsSmall || reachedUrlLengthMax) {
+          var confirmHeading = popup.querySelector(".lb-confirm-message h3");
+          confirmHeading.textContent = `${users.length} ${
+            confirmHeading.textContent
+          }`;
           stopScrolling();
+          popup.classList.add("lb-check");
           initBlocking(users, requestUrl, popup);
         }
       }, 800);
