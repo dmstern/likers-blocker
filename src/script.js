6821237c6815af54686a2e07cd729e688fcbe942
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2020-03-01 17:45:54 +0100

    fix RT param toggle

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ var infoIcon =
   '<svg viewBox="0 0 24 24" class="r-daml9f r-4qtqp9 r-yyyyoo r-1q142lx r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M12 18.042c-.553 0-1-.447-1-1v-5.5c0-.553.447-1 1-1s1 .447 1 1v5.5c0 .553-.447 1-1 1z"></path><circle cx="12" cy="8.042" r="1.25"></circle><path d="M12 22.75C6.072 22.75 1.25 17.928 1.25 12S6.072 1.25 12 1.25 22.75 6.072 22.75 12 17.928 22.75 12 22.75zm0-20C6.9 2.75 2.75 6.9 2.75 12S6.9 21.25 12 21.25s9.25-4.15 9.25-9.25S17.1 2.75 12 2.75z"></path></g></svg>';
 
 var apiUrlBlock = "https://ichbinhier-twittertools.herokuapp.com/blocklists";
+var requestUrl = "";
 var urlLengthMax = 2000;
 var collectedUsers = [];
 var reactRoot;
@@ -280,9 +281,9 @@ function addBlockButton() {
       loadingInfo.appendChild(checkmark);
       checkmark.style.background = highlightColor;
       checkmark.innerHTML = checkmarkIcon;
+
       checkbox.addEventListener("change", () => {
         var tweetParam = checkbox.checked ? `&tweet_id=${tweetId}` : "";
-        var requestUrl = confirmButton.href;
         confirmButton.href = `${requestUrl}${tweetParam}`;
       });
 
@@ -341,7 +342,7 @@ function addBlockButton() {
         addUsers(scrapeUsernames(scrollList));
 
         var users = getUsers();
-        var requestUrl = `${apiUrlBlock}?users=${users}`;
+        requestUrl = `${apiUrlBlock}?users=${users}`;
         var reachedUrlLengthMax = requestUrl.length > urlLengthMax - 100;
         confirmButton.href = requestUrl;
 
