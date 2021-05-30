ebbb0dc439e47d491f4f00f76216211f5c913336
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2021-05-31 01:25:37 +0200

    Change likers limit to 100

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 export default {
 	API_URL_BLOCK: "https://ichbinhier-twittertools.herokuapp.com/blocklists",
 	URL_LENGTH_MAX: 2_000,
-	LIKERS_LIMIT: 80,
+	LIKERS_LIMIT: 100,
 	BEARER_TOKEN: "AAAAAAAAAAAAAAAAAAAAAMLheAAAAAAA0%2BuSeid%2BULvsea4JtiGRiSDSJSI%3DEUifiRBkKG5E2XzMDjRfl76ZC9Ub0wnz4XsNiRVBChTYbJcE3F",
 };
