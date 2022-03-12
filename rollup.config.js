968993794e3f748ea41529d785c66a08a05f279b
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2022-03-12 14:38:54 +0100

    Copy files via rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,29 @@
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import {terser} from "rollup-plugin-terser";
 import sass from 'rollup-plugin-sass';
 import prettier from "rollup-plugin-prettier";
+import copy from 'rollup-plugin-copy'
+
+const targetFolder = "dist";
 
 export default {
-  input: "src/index.ts",
-  output: {
-    dir: "dist",
-    format: "iife"
-  },
-  plugins: [
+	input: "src/index.ts",
+	output: {
+		dir: targetFolder,
+		format: "iife"
+	},
+	plugins: [
 		typescript(),
 		sass({
-			output: "dist/style.css",
+			output: `${targetFolder}/style.css`,
 		}),
 		terser(),
 		prettier({
 			tabWidth: 2,
 			singleQuote: false,
 		}),
-	]
+		copy({
+			targets: [{src: "assets/*", dest: targetFolder}],
+		}),
+	],
 };
