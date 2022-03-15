0ec4f55dde81e46a5607017274479f5cae4c9809
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2022-03-15 20:00:54 +0100

    Fix ide warnings

--- util/release.js
+++ util/release.js
@@ -3,15 +3,11 @@ const fs = require("fs");
 const path = require("path");
 
 function adaptManifestVersion() {
-	const package = require(path.resolve(__dirname, "../package.json"));
+	const packageJson = require(path.resolve(__dirname, "../package.json"));
 	const manifest = require(path.resolve(__dirname, "../assets/manifest.json"));
 
-	let version = package.version;
-	manifest.version = version;
-	fs.writeFileSync(
-		path.resolve(__dirname, "../assets/manifest.json"),
-		JSON.stringify(manifest),
-	);
+	manifest.version = packageJson.version;
+	fs.writeFileSync(path.resolve(__dirname, "../assets/manifest.json"), JSON.stringify(manifest));
 }
 
 adaptManifestVersion();
