2a6f9dda3f230b154a7182b155e9b49326d92b2c
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2022-05-08 20:10:52 +0200

    Fix bug with last rest part of users and cleanup icon code

--- assets/ichbinhier.js
+++ assets/ichbinhier.js
@@ -12,6 +12,8 @@
 		Home: `<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>`,
 		Login: `<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path></svg>`,
 		Logout: `<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path></svg>`,
+		Ban: `<svg width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>`,
+		Check: `<svg class="w-6 h-6" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>`,
 	};
 
 	init();
@@ -57,7 +59,7 @@
 	}
 
 	function blockPart(counter, form, checkboxes) {
-		if (counter > Math.round(checkboxes.length / USERS_PER_REQUEST)) {
+		if (counter > Math.round(checkboxes.length / USERS_PER_REQUEST) + 1) {
 			console.log("All accounts blocked.");
 			document.body.classList.add("all-blocked");
 			document.body.classList.remove("blocking");
@@ -123,10 +125,14 @@
 				formCheck.innerHTML = `
 							<div class="form-check__profile-picture" style="background-image: url(${user.profilePictureUrl});">
 								<img src="${user.profilePictureUrl}" alt="🤡">
-								<svg width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>
+								${icons.Ban}
 							</div>
-							<input class="form-check__input" name="profile_urls" type="checkbox" value="${user.userHandle || user}" id="check-${user.userHandle || user}" checked="">
-							<label class="form-check__label" for="check-${user.userHandle || user}">${user.userHandle || user}</label>`;
+							<input class="form-check__input" name="profile_urls" type="checkbox" value="${
+								user.userHandle || user
+							}" id="check-${user.userHandle || user}" checked="">
+							<label class="form-check__label" for="check-${user.userHandle || user}">${
+					user.userHandle || user
+				}</label>`;
 				return formCheck;
 			});
 		});
@@ -266,7 +272,7 @@
 			<div class="container">${originalContainer.innerHTML}</div>
 			<div class="container">
 				<p class="confirm-message">
-					<svg class="w-6 h-6" width="24" height="24" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
+					${icons.Check}
 					${getLabel("ichbinhier_blockSuccess", "All blocked")}
 				</p>
 			</div>
