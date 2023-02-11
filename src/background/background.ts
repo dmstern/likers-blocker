e5b802d3c730690b0c5ba2bec7d307a0eeefe921
Philip Kreißel <philip.kreissel@icloud.com>
2023-02-11 18:10:44 +0100

    feat: implement alarm task for blocking

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,20 +1,30 @@
 import Storage, { Key } from "../Storage";
+import APIService from "../APIService";
 
 const client = typeof browser === "undefined" ? chrome : browser;
 
 function logURL(e) {
-	console.log("Request")
 	for (const header of e.requestHeaders) {
 		if ((header.name = "authorization") && (header.value.includes("Bearer"))) {
-			console.log("saving token")
+			//console.log("saving token")
 			Storage.set(Key.authorization, header.value);
 		}
 		const re = /[0-9A-Fa-f]{160}/
 		if ((header.name = "x-csrf-token") && re.test(header.value) && (header.value.length == 160)) {
-			console.log("saving csfr")
+			//console.log("saving csfr")
 			Storage.set(Key.csfr, header.value);
 		}
 
+	}
+}
+
+async function blockTask(alarm) {
+	if (alarm.name != "blockTask") return
+	for (let _ of [1, 2, 3]) {
+		const user = await Storage.dequeue()
+		if (!user) return
+		const response = await APIService.block(user);
+		if (response.status != 200) Storage.queue(user)
 	}
 }
 
@@ -23,3 +33,10 @@ client.webRequest.onBeforeSendHeaders.addListener(
 	{ urls: ["<all_urls>"] },
 	["requestHeaders"]
 );
+
+client.alarms.create("blockTask", {
+	delayInMinutes: 1,
+	periodInMinutes: 1,
+});
+
+client.alarms.onAlarm.addListener(blockTask)
