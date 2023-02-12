181e6c090129420a23c8e253300f49ed71cf6099
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-12 23:31:00 +0100

    chore: replace Cookies class with simpler function in Storage

--- src/APIService.ts
+++ src/APIService.ts
@@ -1,4 +1,3 @@
-import Cookies, { Name } from "./Cookies";
 import Storage, { Key } from "./Storage";
 const API_URL = "https://api.twitter.com/1.1/";
 
@@ -9,6 +8,7 @@ export default class APIService {
 		const csrf = (await Storage.get(Key.csfr)) as string;
 		const authorization = (await Storage.get(Key.authorization)) as string;
 		const blocklist = (await Storage.getBlockedAccounts()) as string[];
+		const lang = await Storage.getLanguage();
 
 		if (blocklist.includes(screenName)) {
 			console.warn(`${screenName} is already blocked.`);
@@ -23,7 +23,7 @@ export default class APIService {
 				"Accept-Language": "de-DE,en-US;q=0.7,en;q=0.3", // TODO read from twitter app
 				"Content-Type": "application/x-www-form-urlencoded",
 				"x-twitter-auth-type": "OAuth2Session",
-				"x-twitter-client-language": Cookies.get(Name.lang),
+				"x-twitter-client-language": lang,
 				"x-twitter-active-user": "yes",
 				"x-csrf-token": csrf,
 				"Sec-Fetch-Dest": "empty",
