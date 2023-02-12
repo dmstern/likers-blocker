a20547cbca414208a0e2eb207568f7b68ef87539
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-12 01:51:30 +0100

    chore: Remove ichbinhier heroku references

--- src/content/settings.ts
+++ src/content/settings.ts
@@ -1,7 +1,4 @@
 export default {
-	API_URL_BLOCK: "https://ichbinhier-twittertools.herokuapp.com/blocklists",
-	URL_LENGTH_MAX: 2_048,
-	SMALL_LIST_LIMIT: 200,
 	SCROLL_INTERVAL: 1_600,
 	IDLE_COUNTER_ALLOWANCE: 2,
 };
