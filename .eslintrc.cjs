76780d79d80470bdb60b5d0a9c7d7eaa9e54a762
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-18 01:41:30 +0100

    feat: warn about prettier issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,50 +1,38 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	env: {
+		browser: true,
+		es2021: true,
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended"
-	],
-	"overrides": [
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
-	"plugins": [
-		"@typescript-eslint",
-		"prettier",
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"linebreak-style": ["error", "unix"],
+		"comma-dangle": [
+			"warn",
+			{
+				arrays: "always-multiline",
+				objects: "always-multiline",
+			},
 		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
+		quotes: ["error", "double", { avoidEscape: true }],
+		semi: ["error", "always"],
+		"prettier/prettier": "warn",
 		"no-restricted-syntax": [
 			"error",
 			{
-				"selector": "IfStatement > ExpressionStatement > AssignmentExpression",
-				"message": "Please avoid single-line if syntax."
+				selector: "IfStatement > ExpressionStatement > AssignmentExpression",
+				message: "Please avoid single-line if syntax.",
 			},
 			{
-				"selector": "IfStatement > ReturnStatement",
-				"message": "Please avoid single-line if syntax."
-			}
+				selector: "IfStatement > ReturnStatement",
+				message: "Please avoid single-line if syntax.",
+			},
 		],
-	}
+	},
 };
