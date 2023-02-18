35d0b4af9269ddb62c123428dcb6071ade8fb06d
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-18 04:30:35 +0100

    fix: allow background to send messages to inactive twitter tab

--- src/Tabs.ts
+++ src/Tabs.ts
@@ -1,7 +1,7 @@
 import { tabs } from "webextension-polyfill";
 
 export async function getTwitterTab() {
-	const activeTabs = await tabs.query({ active: true, currentWindow: true });
+	const activeTabs = await tabs.query({ currentWindow: true });
 	const twitterTab = activeTabs.find((tab) => tab.url.includes("twitter.com"));
 	console.debug("found twitterTab: ", twitterTab);
 	return twitterTab;
