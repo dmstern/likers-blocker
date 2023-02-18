493d50acb28983b6a21ab275772eba8e242b7092
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-18 02:12:16 +0100

    feat: minify badge text for large queue length

--- src/Badge.ts
+++ src/Badge.ts
@@ -7,7 +7,25 @@ export default class Badge {
 
 	static async updateBadgeCount(length: number) {
 		console.log("updateBadgeCount");
-		const text = length > 0 ? length.toString() : "";
+
+		const minifyProperties = [
+			{
+				suffix: "M",
+				divisor: 1_000_000,
+			},
+			{
+				suffix: "k",
+				divisor: 1_000,
+			},
+			{
+				suffix: "",
+				divisor: 1,
+			},
+		];
+
+		const { suffix, divisor } = minifyProperties.find(({ divisor }) => length >= divisor);
+		const prefix = suffix.length > 0 ? ">" : "";
+		const text = length == 0 ? "" : `${prefix}${Math.floor(length / divisor)}${suffix}`;
 		const details = {
 			text: text,
 		};
