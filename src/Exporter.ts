a859b404e8a73b4f3bb9760794d8f5aeba780a5b
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-19 23:55:53 +0100

    fix: error in popup if blocklist is empty

--- src/Exporter.ts
+++ src/Exporter.ts
@@ -8,19 +8,17 @@ export default class Exporter {
 		data: string;
 		encoding: string;
 	} {
-		if (!users.size) {
-			return;
-		}
-
 		// single column CSV file
 		const filename = "blocklist.csv";
-		const data = users
-			.toArray()
-			.map((user) => {
-				const { screen_name, interacted_with } = user;
-				return `${screen_name},https://twitter.com${interacted_with}`;
-			})
-			.join("\n");
+		const data = users.size
+			? users
+					.toArray()
+					.map((user) => {
+						const { screen_name, interacted_with } = user;
+						return `${screen_name},https://twitter.com${interacted_with}`;
+					})
+					.join("\n")
+			: "";
 		const mimeType = "text/csv";
 		const encoding = "utf-8";
 		const url = `data:${mimeType};charset=${encoding},${encodeURIComponent(data)}`;
