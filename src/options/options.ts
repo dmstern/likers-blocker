e02f50b3390e9fb972fb1ed1b18d4f2311632b98
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-20 22:04:02 +0100

    feat: add success and error message for import

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,3 +1,4 @@
+import { i18n } from "webextension-polyfill";
 import { injectIcons } from "../content/icons";
 import FileManager from "../FileManager";
 import { localizeUI } from "../Localization";
@@ -9,6 +10,32 @@ import "./options.scss";
 
 	const importListButton = document.querySelector("#importBlockList");
 	const downloadButton = document.querySelector("#downloadBlockList");
-	importListButton?.addEventListener("click", FileManager.importBlockList);
+	const statusMessage = document.querySelector("#statusMessage");
+	const errorDetails = statusMessage.querySelector(".details");
+	const statusMessageSummary = statusMessage.querySelector("summary");
+
+	console.log(statusMessageSummary);
+
+	importListButton?.addEventListener("click", () => {
+		statusMessage.classList.remove("success");
+		statusMessage.classList.remove("error");
+		statusMessageSummary.innerHTML = "";
+		errorDetails.innerHTML = "";
+
+		FileManager.importBlockList()
+			.then(() => {
+				statusMessage.classList.add("success");
+				statusMessageSummary.innerHTML = i18n.getMessage("options_import_success");
+				setTimeout(() => {
+					statusMessage.classList.remove("success");
+				}, 5000);
+			})
+			.catch((error) => {
+				statusMessage.classList.add("error");
+				statusMessageSummary.innerHTML = i18n.getMessage("options_import_error");
+				errorDetails.innerHTML = error;
+			});
+	});
+
 	downloadButton.addEventListener("click", FileManager.downloadBlockList);
 })();
