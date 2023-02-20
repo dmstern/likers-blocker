ecf6173345241ac6523c93412174daecf8dfa23c
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-20 11:48:11 +0100

    feat: add i18n to import / export / options / options button

---
+++ src/Localization.ts
@@ -0,0 +1,28 @@
+import { i18n } from "webextension-polyfill";
+
+function replaceData(dataName: string, callback: (element: HTMLElement, message: string) => void) {
+	const elements = document.querySelectorAll(`[data-${dataName}]`) as NodeListOf<HTMLElement>;
+	elements.forEach((element: HTMLElement) => {
+		const messageName = element.dataset[dataName];
+
+		if (!messageName) {
+			return;
+		}
+
+		callback(element, i18n.getMessage(messageName));
+	});
+}
+
+export function localizeUI() {
+	replaceData("label", (element, message) => {
+		element.innerHTML = message;
+	});
+
+	replaceData("href", (element, message) => {
+		element.setAttribute("href", message);
+	});
+
+	replaceData("title", (element, message) => {
+		element.setAttribute("title", message);
+	});
+}
