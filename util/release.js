db070c3214566047aa027bc0f632ca4e2ad1d57c
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-21 15:17:00 +0100

    fix: adapt release task to node 16

--- util/release.js
+++ util/release.js
@@ -1,13 +1,21 @@
 // @ts-check
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 function adaptManifestVersion() {
-	const packageJson = require(path.resolve(__dirname, "../package.json"));
-	const manifest = require(path.resolve(__dirname, "../assets/manifest.json"));
+	const packageJsonFile = fs.readFileSync(path.resolve(__dirname, "../package.json"));
+	const manifestFile = fs.readFileSync(path.resolve(__dirname, "../assets/manifest.json"));
+	const packageJson = JSON.parse(packageJsonFile.toString());
+	const manifest = JSON.parse(manifestFile.toString());
 
 	manifest.version = packageJson.version;
-	fs.writeFileSync(path.resolve(__dirname, "../assets/manifest.json"), JSON.stringify(manifest));
+	fs.writeFileSync(
+		path.resolve(__dirname, "../assets/manifest.json"),
+		JSON.stringify(manifest, null, "\t")
+	);
 }
 
 adaptManifestVersion();
