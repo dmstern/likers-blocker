a9cb17c5c41a3e1a0616a54dad0a772ca3aa369f
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-22 21:51:17 +0100

    try to apply block speed update immediately

--- src/Messages.ts
+++ src/Messages.ts
@@ -5,6 +5,7 @@ import { User } from "./UserInfo";
 enum Action {
 	getUserInfo = "getUserInfo",
 	queueUpdate = "queueUpdate",
+	// blockSpeedUpdate = "blockSpeedUpdate",
 }
 
 interface Message {
@@ -34,6 +35,25 @@ export interface GetUserInfoResponse {
 }
 
 export default class Messenger {
+	// static async sendBlockSpeedUpdate() {
+	// 	const action = Action.blockSpeedUpdate;
+
+	// 	try {
+	// 		await runtime.sendMessage({ action });
+	// 	} catch (error) {
+	// 		console.info("✉ Message was send but no receiver listens to it.", action);
+	// 	}
+	// }
+
+	// static async addBlockSpeedUpdateListener(callback: () => void) {
+	// 	runtime.onMessage.addListener((message: Message) => {
+	// 		console.log("✉ message from background", message);
+	// 		if (message.action === Action.blockSpeedUpdate) {
+	// 			callback();
+	// 		}
+	// 	});
+	// }
+
 	static async sendGetUserInfoMessage(): Promise<GetUserInfoResponse> {
 		const twitterTab = await getTwitterTab();
 		const action = Action.getUserInfo;
