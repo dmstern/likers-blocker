f81e8eae3e794119be4c81177c52d5be72ffa7e4
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-02-27 12:43:49 +0100

    fix: progressbar label color for light themes

--- src/popup/BlockSpeedometer.ts
+++ src/popup/BlockSpeedometer.ts
@@ -1,4 +1,4 @@
-import { i18n } from "webextension-polyfill";
+import { i18n, runtime } from "webextension-polyfill";
 import Storage from "../Storage";
 import "../sass-commons/speedometer-icon.scss";
 import settings from "../settings";
@@ -17,20 +17,25 @@ export default class BlockSpeedometer {
 		document.body.style.setProperty("--block-speed-max", settings.BLOCKS_PER_MINUTE_MAX.toString());
 		document.body.classList.toggle("blocker-sleeping", blockSpeed < 1);
 
-		if (speedometer && label) {
-			const labelText = i18n.getMessage(label.dataset.label, blockSpeed.toString());
-			if (blockSpeed > 0) {
-				label.innerHTML = labelText
-					.split(" ")
-					.map(
-						(word) =>
-							`<span class="${/\d/.test(word) ? "block-speedometer__label" : ""}">${word}</span>`
-					)
-					.join("&nbsp;");
-				label.style.setProperty("--hue", `${(blockSpeed * 100) / -60 + 100} `);
-			} else {
-				label.innerHTML = labelText;
-			}
+		if (!speedometer || !label) {
+			return;
+		}
+
+		speedometer.addEventListener("click", () => {
+			runtime.openOptionsPage();
+		});
+
+		const labelText = i18n.getMessage(label.dataset.label, blockSpeed.toString());
+		if (blockSpeed > 0) {
+			label.innerHTML = labelText
+				.split(" ")
+				.map(
+					(word) => `<span class="${/\d/.test(word) ? "block-speedometer__label" : ""}">${word}</span>`
+				)
+				.join("&nbsp;");
+			label.style.setProperty("--hue", `${(blockSpeed * 100) / -60 + 100} `);
+		} else {
+			label.innerHTML = labelText;
 		}
 	}
 }
