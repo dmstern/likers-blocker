b6ca54cd54dc0ed8346835e10edffb95969e387f
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-03-01 21:57:06 +0100

    fix: increase chance to get a twitter tab after logout

--- src/Tabs.ts
+++ src/Tabs.ts
@@ -1,8 +1,8 @@
-// import { tabs } from "webextension-polyfill";
+import { tabs } from "webextension-polyfill";
 
-// export async function getTwitterTab() {
-// 	const activeTabs = await tabs.query({ currentWindow: true });
-// 	const twitterTab = activeTabs.find((tab) => tab.url.includes("twitter.com"));
-// 	console.debug("found twitterTab: ", twitterTab);
-// 	return twitterTab;
-// }
+export async function getTwitterTab() {
+	const activeTabs = await tabs.query({ currentWindow: true });
+	const twitterTab = activeTabs.reverse().find((tab) => tab.url.includes("twitter.com"));
+	console.debug("found twitterTab: ", twitterTab);
+	return twitterTab;
+}
